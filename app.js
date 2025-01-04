af1c1f9cea9cdefe350544adecb0ec49eef7b06d
Archit Madankar <architmadankar21@gmail.com>
2025-01-04 16:12:45 +0530

    Add Text in app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!<br>This app was created by Archit Madankar for the DevOps Intern Task for Swayatt Drishtigochar.'));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
